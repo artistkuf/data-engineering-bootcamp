168583067ff59d34b9225608e94cd1a1f64ac4f3
Wasin Mekkit <Wasinmekkit@gmail.com>
2026-08-30 10:03:39 +0000

    setup airflow + spark

--- 04-automated-data-pipelines/pyspark/demo_gcs.py
+++ 04-automated-data-pipelines/pyspark/demo_gcs.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 
 
-KEYFILE_PATH = "/opt/spark/pyspark/YOUR_KEYFILE.json"
+KEYFILE_PATH = "/opt/spark/pyspark/deb-wasin-key.json"
 
 # GCS Connector Path (on Spark): /opt/spark/jars/gcs-connector-hadoop3-latest.jar
 # GCS Connector Path (on Airflow): /home/airflow/.local/lib/python3.9/site-packages/pyspark/jars/gcs-connector-hadoop3-latest.jar
@@ -22,7 +22,7 @@ spark = SparkSession.builder.appName("demo_gcs") \
     .config("google.cloud.auth.service.account.json.keyfile", KEYFILE_PATH) \
     .getOrCreate()
 
-GCS_FILE_PATH = "gs://YOUR_BUCKET_PATH_TO_CSV_FILE"
+GCS_FILE_PATH = "gs://deb-bootcamp-03/raw/greenery/addresses/addresses.csv"
 df = spark.read.option("header", True).csv(GCS_FILE_PATH)
 df.show()
 
