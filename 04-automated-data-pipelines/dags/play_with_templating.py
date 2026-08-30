4e72c428263ee67d659f793216d0f3c8286aa147
Wasin Mekkit <Wasinmekkit@gmail.com>
2026-08-30 07:46:08 +0000

    update: backfill data

--- 04-automated-data-pipelines/dags/play_with_templating.py
+++ 04-automated-data-pipelines/dags/play_with_templating.py
@@ -1,19 +1,17 @@
 from airflow import DAG
 from airflow.macros import ds_format
-from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.utils import timezone
 
 
 def _get_date_part(ds, **context):
-    print(ds)
-    return ds_format(ds, "%Y-%m-%d", "%Y/%m/%d/")
+	return ds_format(ds, "%Y-%m-%d", "%Y/%m/%d/")
 
 
 with DAG(
     dag_id="play_with_templating",
-    schedule=None,
-    start_date=timezone.datetime(2024, 3, 10),
+    schedule="@daily",
+    start_date=timezone.datetime(2025, 5, 25),
     catchup=False,
     tags=["DEB", "Skooldio"],
 ):
@@ -22,14 +20,4 @@ with DAG(
         task_id="get_date_part",
         python_callable=_get_date_part,
         op_kwargs={"ds": "{{ ds }}"},
-    )
-
-    echo = BashOperator(
-        task_id="echo",
-        bash_command="echo {{ logical_date }}",
-    )
-    
-    var = BashOperator(
-        task_id="var",
-        bash_command="echo {{ var.value.name }}",
     )
